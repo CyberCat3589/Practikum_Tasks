4cffdb00cfc74b01674dbc4e9094541ad96a2010
CyberCat <Vgnetnet156@gmail.com>
2023-06-22 16:25:30 +0300

    Creating AddNewTask & GetPersonTasksInfo functions

---
+++ Lesson 7 Real types and the tasks of tasks/main.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+// Перечислимый тип для статуса задачи
+enum class TaskStatus {
+    NEW,          // новая
+    IN_PROGRESS,  // в разработке
+    TESTING,      // на тестировании
+    DONE          // завершена
+};
+
+// Объявляем тип-синоним для map<TaskStatus, int>,
+// позволяющего хранить количество задач каждого статуса
+using TasksInfo = map<TaskStatus, int>;
+
+class TeamTasks {
+public:
+    // Получить статистику по статусам задач конкретного разработчика
+    const TasksInfo& GetPersonTasksInfo(const string& person) const
+    {
+        TasksInfo tasks_info = person_tasks.at(person);
+        return tasks_info;
+    }
+
+    // Добавить новую задачу (в статусе NEW) для конкретного разработчитка
+    void AddNewTask(const string& person)
+    {
+        //person_tasks[person].insert({TaskStatus::NEW, 1});
+        person_tasks[person][TaskStatus::NEW] += 1;
+    }
+
+    // Обновить статусы по данному количеству задач конкретного разработчика,
+    // подробности см. ниже
+    tuple<TasksInfo, TasksInfo> PerformPersonTasks(const string& person, int task_count)
+    {
+        TasksInfo tasks_info = person_tasks.at(person);
+        
+    }
+
+private:
+    map<string, TasksInfo> person_tasks;
+};
+
+// Принимаем словарь по значению, чтобы иметь возможность
+// обращаться к отсутствующим ключам с помощью [] и получать 0,
+// не меняя при этом исходный словарь.
+void PrintTasksInfo(TasksInfo tasks_info) {
+    cout << tasks_info[TaskStatus::NEW] << " new tasks"s
+         << ", "s << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress"s
+         << ", "s << tasks_info[TaskStatus::TESTING] << " tasks are being tested"s
+         << ", "s << tasks_info[TaskStatus::DONE] << " tasks are done"s << endl;
+}
+
+int main() {
+    TeamTasks tasks;
+    tasks.AddNewTask("Ilia"s);
+    for (int i = 0; i < 3; ++i) {
+        tasks.AddNewTask("Ivan"s);
+    }
+    cout << "Ilia's tasks: "s;
+    PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"s));
+    cout << "Ivan's tasks: "s;
+    PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"s));
+
+    TasksInfo updated_tasks, untouched_tasks;
+
+    tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan"s, 2);
+    cout << "Updated Ivan's tasks: "s;
+    PrintTasksInfo(updated_tasks);
+    cout << "Untouched Ivan's tasks: "s;
+    PrintTasksInfo(untouched_tasks);
+
+    tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan"s, 2);
+    cout << "Updated Ivan's tasks: "s;
+    PrintTasksInfo(updated_tasks);
+    cout << "Untouched Ivan's tasks: "s;
+    PrintTasksInfo(untouched_tasks);
+}
