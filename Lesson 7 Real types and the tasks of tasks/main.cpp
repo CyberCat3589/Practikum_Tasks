152b8ef7c058e9ad64bcb2618ebaf9e762c831b5
CyberCat <Vgnetnet156@gmail.com>
2023-06-26 16:54:56 +0300

    Start

--- Lesson 7 Real types and the tasks of tasks/main.cpp
+++ Lesson 7 Real types and the tasks of tasks/main.cpp
@@ -23,27 +23,74 @@ public:
     // Получить статистику по статусам задач конкретного разработчика
     const TasksInfo& GetPersonTasksInfo(const string& person) const
     {
-        TasksInfo tasks_info = person_tasks.at(person);
-        return tasks_info;
+        return person_info_.at(person);
+    }
+
+    TaskStatus GetNextStatus(const TaskStatus current_status)
+    {
+        switch(current_status)
+        {
+            case TaskStatus::NEW:
+            return TaskStatus::IN_PROGRESS;
+            break;
+
+            case TaskStatus::IN_PROGRESS:
+            return TaskStatus::TESTING;
+            break;
+
+            case TaskStatus::TESTING:
+            return TaskStatus::DONE;
+            break;
+
+            case TaskStatus::DONE:
+            return TaskStatus::DONE;
+            break;
+
+            default:
+            return TaskStatus::NEW;
+            break;
+        } 
     }
 
     // Добавить новую задачу (в статусе NEW) для конкретного разработчитка
     void AddNewTask(const string& person)
     {
         //person_tasks[person].insert({TaskStatus::NEW, 1});
-        person_tasks[person][TaskStatus::NEW] += 1;
+        person_info_[person][TaskStatus::NEW] += 1;
     }
 
     // Обновить статусы по данному количеству задач конкретного разработчика,
     // подробности см. ниже
     tuple<TasksInfo, TasksInfo> PerformPersonTasks(const string& person, int task_count)
     {
-        TasksInfo tasks_info = person_tasks.at(person);
+        TasksInfo tasks_to_update, untouched_tasks;
+
+        if(person_info_.at(person).empty()) return tuple(tasks_to_update, untouched_tasks);
         
+        for(auto &[status, count] : person_info_.at(person))
+        {
+            if(status == TaskStatus::DONE) break;
+            
+            
+        }
+ 
+        /*for(auto &[status, count] : person_info_.at(person))
+        {
+            if(untouched_tasks.count(status))
+            {
+                count = untouched_tasks[status];
+            } 
+            else if(tasks_to_update.count(status))
+            {
+                count += tasks_to_update[status];
+            }
+        }*/
+
+        return tuple(tasks_to_update, untouched_tasks);
     }
 
 private:
-    map<string, TasksInfo> person_tasks;
+    map<string, TasksInfo> person_info_;
 };
 
 // Принимаем словарь по значению, чтобы иметь возможность
@@ -58,26 +105,26 @@ void PrintTasksInfo(TasksInfo tasks_info) {
 
 int main() {
     TeamTasks tasks;
-    tasks.AddNewTask("Ilia"s);
-    for (int i = 0; i < 3; ++i) {
-        tasks.AddNewTask("Ivan"s);
-    }
-    cout << "Ilia's tasks: "s;
-    PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"s));
+    
+    tasks.AddNewTask("Ivan"s);
+
     cout << "Ivan's tasks: "s;
     PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"s));
 
     TasksInfo updated_tasks, untouched_tasks;
 
-    tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan"s, 2);
+    tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan"s, 1000);
     cout << "Updated Ivan's tasks: "s;
     PrintTasksInfo(updated_tasks);
-    cout << "Untouched Ivan's tasks: "s;
-    PrintTasksInfo(untouched_tasks);
+    // cout << "Untouched Ivan's tasks: "s;
+    // PrintTasksInfo(untouched_tasks);
 
-    tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan"s, 2);
-    cout << "Updated Ivan's tasks: "s;
-    PrintTasksInfo(updated_tasks);
-    cout << "Untouched Ivan's tasks: "s;
-    PrintTasksInfo(untouched_tasks);
+    // tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan"s, 2);
+    // cout << "Updated Ivan's tasks: "s;
+    // PrintTasksInfo(updated_tasks);
+    // cout << "Untouched Ivan's tasks: "s;
+    // PrintTasksInfo(untouched_tasks);
+
+    // cout << "Ivan's tasks: "s;
+    PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"s));
 }
