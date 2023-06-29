d6dffc4ff394b05ba329787739be3a1008d4ed96
CyberCat <Vgnetnet156@gmail.com>
2023-06-29 23:51:57 +0300

    All tests passed

--- Lesson 4 How templates work/main.cpp
+++ Lesson 4 How templates work/main.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
+#include <algorithm>
 
 using namespace std;
 
-template <typename Documents, typename term>
-vector<double> ComputeTfIdfs(Documents, term)
+template <typename Documents, typename Term>
+vector<double> ComputeTfIdfs(const Documents& documents, const Term& term)
 {
     vector<double> tf_idfs;
     double tf = 0;
-    int document_freq;
+    double idf = 0;
+    int document_freq = 0;
 
-    for(const auto& document : Documents)
+    for(const auto& document : documents)
     {
+        if(count(document.begin(), document.end(), term) > 0) ++document_freq;
+    }
+
+    for(const auto& document : documents)
+    {
+         
         for(const auto& word : document)
         {
             if(word == term)
             {
                 tf += 1.0 / document.size();
-                document_freq = 1;
             }
-            
         }
-        // here
+
+        idf = log(static_cast<double>(documents.size()) / document_freq);
+
+        tf_idfs.push_back(tf * idf);
+        tf = 0;
     }
 
     return tf_idfs;
