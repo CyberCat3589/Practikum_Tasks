e13d0c982882d5a49aa9decef48b21de5d79146c
CyberCat <Vgnetnet156@gmail.com>
2023-06-30 10:25:07 +0300

    Creating function for sets

--- Lesson 5 Universal container output functions/main.cpp
+++ Lesson 5 Universal container output functions/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <set>
 
 using namespace std;
 
+// для векторов
 template <typename Term>
 ostream& operator<<(ostream& out, const vector<Term>& container)
 {
@@ -24,9 +26,35 @@ ostream& operator<<(ostream& out, const vector<Term>& container)
     return out;
 }
 
+// для множеств
+template <typename Term>
+ostream& operator<<(ostream& out, const set<Term>& container)
+{
+    bool first_iter = true;
+
+    for(auto element : container)
+    {
+        if(first_iter)
+        {
+            out << element;
+            first_iter = false;
+        }
+        else
+        {
+            out << ", "s << element;
+        }
+    }
+
+    return out;
+}
+
 int main() 
 {
-    const vector<int> ages = {10, 5, 2, 12};
-    cout << ages << endl;
+    // const vector<int> ages = {10, 5, 2, 12};
+    // cout << ages << endl;
+
+    const set<string> cats = {"Мурка"s, "Белка"s, "Георгий"s, "Рюрик"s};
+    cout << cats << endl;
+
     return 0;
 }
