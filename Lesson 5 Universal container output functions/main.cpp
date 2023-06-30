c3386b7be8a228f54c1335c1a2cc2852766ed801
CyberCat <Vgnetnet156@gmail.com>
2023-06-30 12:13:22 +0300

    Stage 6

--- Lesson 5 Universal container output functions/main.cpp
+++ Lesson 5 Universal container output functions/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <map>
 
 using namespace std;
 
+// переопределение оператора << для функции Print
+template <typename First, typename Second>
+ostream& operator<<(ostream& out, const pair<First, Second>& pair)
+{
+    return out << '(' << pair.first << ", " << pair.second << ')';
+}
+
+// вывод элементов контейнера
 template <typename Container>
 void Print(ostream& out, const Container& container)
 {
@@ -43,12 +52,29 @@ ostream& operator<<(ostream& out, const set<Term>& container)
     return out;
 }
 
+// для словарей
+template <typename Element1, typename Element2>
+ostream& operator<<(ostream& out, const map<Element1, Element2>& container)
+{
+    out << '<';
+    Print(out, container); 
+    out << '>';
+    return out;
+}
+
 int main() 
 {
     const set<string> cats = {"Мурка"s, "Белка"s, "Георгий"s, "Рюрик"s};
     cout << cats << endl;
     const vector<int> ages = {10, 5, 2, 12};
     cout << ages << endl;
+    const map<string, int> cat_ages = {
+        {"Мурка"s, 10}, 
+        {"Белка"s, 5},
+        {"Георгий"s, 2}, 
+        {"Рюрик"s, 12}
+    };
+    cout << cat_ages << endl;
 
     return 0;
 }
