40d4091153b9e6301c4cdb1816368aef86f80451
CyberCat <Vgnetnet156@gmail.com>
2023-07-03 22:51:09 +0300

    Tests passed

--- Lesson 3 Testing according to plan/main.cpp
+++ Lesson 3 Testing according to plan/main.cpp
@@ -3,20 +3,42 @@
 
 using namespace std;
 
+string RemoveSpaces(const string& text) {
+    string words;
+    string word;
+    for (const char c : text) {
+        if (c == ' ') {
+            if (!word.empty()) {
+                words += word;
+                word.clear();
+            }
+        } else {
+            word += c;
+        }
+    }
+    if (!word.empty()) {
+        words += word;
+    }
+
+    return words;
+}
+
 // Определяет, будет ли слово палиндромом
 // text может быть строкой, содержащей строчные буквы английского алфавита и пробелы
 // Пустые строки и строки, состоящие только из пробелов, - это не палиндромы
 bool IsPalindrome(const string& text) {
     // Напишите недостающий код
-    bool is_palindrome;
-    int str_size = text.size() - 1;
+    string rev_str;
 
-    for(int i = 0; i <= str_size; ++i)
+    string temp = RemoveSpaces(text);
+    if(temp.empty()) return false;
+
+    for(int i = temp.size() - 1; i >= 0; --i)
     {
-        
+        rev_str += temp[i];
     }
 
-    return false;
+    return temp == rev_str;
 }
 
 int main() {
