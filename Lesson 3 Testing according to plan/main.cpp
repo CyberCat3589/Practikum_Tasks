4622f0ae2ff0224402e6e420ac5c909f7045ff3a
CyberCat <Vgnetnet156@gmail.com>
2023-07-03 16:30:34 +0300

    Init

---
+++ Lesson 3 Testing according to plan/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Определяет, будет ли слово палиндромом
+// text может быть строкой, содержащей строчные буквы английского алфавита и пробелы
+// Пустые строки и строки, состоящие только из пробелов, - это не палиндромы
+bool IsPalindrome(const string& text) {
+    // Напишите недостающий код
+    bool is_palindrome;
+    int str_size = text.size() - 1;
+
+    for(int i = 0; i <= str_size; ++i)
+    {
+        
+    }
+
+    return false;
+}
+
+int main() {
+    string text;
+    getline(cin, text);
+
+    if (IsPalindrome(text)) {
+        cout << "palindrome"s << endl;
+    } else {
+        cout << "not a palindrome"s << endl;
+    }
+}
