ca7d412876303a50bee235cdceeb643caef5c748
CyberCat <Vgnetnet156@gmail.com>
2023-07-15 21:08:43 +0300

    Init

---
+++ Lesson 6 Working with cerr/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template </*напишите недостающий код*/>
+void RunTestImpl(/*Напишите недостающий код*/) 
+{
+    /* Напишите недостающий код */
+}
+
+#define RUN_TEST(func)  // напишите недостающий код
+
+void Test1() 
+{
+    
+}
+
+int main() 
+{
+    RUN_TEST(Test1);
+}
