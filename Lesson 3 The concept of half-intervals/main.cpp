46f92a90530b650af7925487d70e30a52c5cd1e1
CyberCat <Vgnetnet156@gmail.com>
2023-07-28 10:39:01 +0300

    Added FindAndPrint function

--- Lesson 3 The concept of half-intervals/main.cpp
+++ Lesson 3 The concept of half-intervals/main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <set>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 template <typename It>
 void PrintRange(It range_begin, It range_end)
 {
-    if(range_begin == range_end) return;
+    if(range_begin == range_end)
+    {
+        cout << endl;
+        return;
+    }
 
     for (auto it = range_begin; it != range_end; ++it) 
     {
@@ -17,13 +22,32 @@ void PrintRange(It range_begin, It range_end)
     cout << endl;
 }
 
+template <typename Container, typename Element>
+void FindAndPrint(Container container, Element element)
+{
+    auto it = find(container.begin(), container.end(), element);
+
+    PrintRange(container.begin(), it);
+    PrintRange(it, container.end());
+}
+
+
 int main() 
 {
+    set<int> test = {1, 1, 1, 2, 3, 4, 5, 5};
     cout << "Test1"s << endl;
-    set<int> test1 = {1, 1, 1, 2, 3, 4, 5, 5};
-    PrintRange(test1.begin(), test1.end());
+    FindAndPrint(test, 3);
+
     cout << "Test2"s << endl;
-    vector<int> test2 = {}; // пустой контейнер
-    PrintRange(test2.begin(), test2.end());
-    cout << "End of tests"s << endl;
+    FindAndPrint(test, 0); // элемента 0 нет в контейнере
+
+    cout << "Test3"s << endl;
+    vector<int> test_vec = {1, 2, 3, 4, 5};
+    FindAndPrint(test_vec, 4);
+
+    cout << "Test4"s << endl;
+    string test_str = "Focus-pokus";
+    FindAndPrint(test_str, '-');
+
+    cout<< "End of tests"s << endl;
 }
