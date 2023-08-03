8ffb004632992d5c4e40486ff842be41283a84c8
CyberCat <Vgnetnet156@gmail.com>
2023-08-03 11:02:45 +0300

    Init task

---
+++ Hanoi Towers/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+
+using namespace std;
+
+class Tower {
+public:
+    // конструктор и метод SetDisks нужны, чтобы правильно создать башни
+    Tower(int disks_num) {
+        FillTower(disks_num);
+    }
+
+    int GetDisksNum() const {
+        return disks_.size();
+    }
+
+    void SetDisks(int disks_num) {
+        FillTower(disks_num);
+    }
+
+    // добавляем диск на верх собственной башни
+    // обратите внимание на исключение, которое выбрасывается этим методом
+    void AddToTop(int disk) {
+        int top_disk_num = disks_.size() - 1;
+        if (0 != disks_.size() && disk >= disks_[top_disk_num]) {
+            throw invalid_argument("Невозможно поместить большой диск на маленький");
+        } else {
+            // допишите этот метод и используйте его в вашем решении
+        }
+    }
+
+    // вы можете дописывать необходимые для вашего решения методы
+
+private:
+    vector<int> disks_;
+
+    // используем приватный метод FillTower, чтобы избежать дубликации кода
+    void FillTower(int disks_num) {
+        for (int i = disks_num; i > 0; i--) {
+            disks_.push_back(i);
+        }
+    }
+};
+
+void SolveHanoi(vector<Tower>& towers) {
+    int disks_num = towers[0].GetDisksNum();
+
+    // допишите функцию, чтобы на towers[0] было 0 дисков,
+    // на towers[1] 0 дисков,
+    // и на towers[2] было disks_num дисков
+}
