36ac08f1b3563d39036675f606e2c06772408421
CyberCat <Vgnetnet156@gmail.com>
2023-08-14 12:58:56 +0300

    Modified main function

--- Lesson 8 Standard algorithms from algorithm/main.cpp
+++ Lesson 8 Standard algorithms from algorithm/main.cpp
@@ -67,6 +67,20 @@ int main()
         cout << s;
     }
     */
+
+   vector<int> test_vector(10);
+    // iota             -> http://ru.cppreference.com/w/cpp/algorithm/iota
+    // Заполняет диапазон последовательно возрастающими значениями
+    iota(test_vector.begin(), test_vector.end(), 1);
+    // random_shuffle   -> https://ru.cppreference.com/w/cpp/algorithm/random_shuffle
+    // Перемешивает элементы в случайном порядке
+    random_shuffle(test_vector.begin(), test_vector.end());
+    // Выводим вектор до сортировки
+    PrintRange(test_vector.begin(), test_vector.end());
+    // Сортируем вектор с помощью сортировки слиянием
+    MergeSort(test_vector.begin(), test_vector.end());
+    // Выводим результат
+    PrintRange(test_vector.begin(), test_vector.end());
    
     return 0;
 }
