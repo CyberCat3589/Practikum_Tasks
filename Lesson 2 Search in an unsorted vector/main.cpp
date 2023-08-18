1723ef6dd9bf51a0582c6d598f23e3f0cf0d6cda
CyberCat <Vgnetnet156@gmail.com>
2023-08-18 12:10:24 +0300

    Task done

--- Lesson 2 Search in an unsorted vector/main.cpp
+++ Lesson 2 Search in an unsorted vector/main.cpp
@@ -9,8 +9,15 @@ void PrintSpacesPositions(string& str)
     auto iter_pos = find(str.begin(), str.end(), ' ');
     while(iter_pos != str.end())
     {
-        
+        cout << distance(str.begin(), iter_pos) << endl;
+        iter_pos = find(next(iter_pos, 1), str.end(), ' ');
     }
+
+    /*
+    for (auto it = find(str.begin(), str.end(), ' '); it != str.end(); it = find(next(it), str.end(), ' ')) 
+    {
+        cout << distance(str.begin(), it) << endl;
+    }*/
 }
 
 int main() 
