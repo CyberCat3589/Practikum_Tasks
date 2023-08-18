96ad9247a2ff5bae7fccb3f03b5ccf755a1d634e
CyberCat <Vgnetnet156@gmail.com>
2023-08-18 15:31:25 +0300

    Task done

--- Lesson 4 Searching in a sorted vector, Dictionary and Set/main.cpp
+++ Lesson 4 Searching in a sorted vector, Dictionary and Set/main.cpp
@@ -6,7 +6,19 @@ using namespace std;
 set<int>::const_iterator FindNearestElement(const set<int>& numbers, int border) 
 {
     // set<int>::const_iterator — тип итераторов для константного множества целых чисел
-    
+    auto it_big = numbers.lower_bound(border);
+    if(it_big != numbers.begin())
+    {
+        auto it_small = prev(it_big, 1);
+        if(abs(border - *it_big) >= abs(border - *it_small))
+        {
+            return it_small;
+        }else
+        {
+            return it_big;
+        }
+    }
+    return it_big;
 }
 
 int main() 
