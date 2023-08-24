260de5f1a945353aaa9e997cbd131cebe7e4c986
CyberCat <Vgnetnet156@gmail.com>
2023-08-24 10:38:55 +0300

    Task Init

---
+++ Lesson 3 Turn and Dec/main.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdint>
+#include <numeric>
+
+using namespace std;
+
+template <typename It>
+void PrintRange(It range_begin, It range_end) 
+{
+    for (auto it = range_begin; it != range_end; ++it) 
+    {
+        cout << *it << " "s;
+    }
+    cout << endl;
+}
+
+template <typename Type>
+class Stack 
+{
+public:
+
+    void Push(const Type& element) 
+    {
+        elements_.push_back(element);
+    }
+
+    void Pop() 
+    {
+        elements_.pop_back();
+    }
+
+    const Type& Peek() const 
+    {
+        return elements_.back();
+    }
+
+    Type& Peek() 
+    {
+        return elements_.back();
+    }
+
+    void Print() const 
+    {
+        PrintRange(elements_.begin(), elements_.end());
+    }
+
+    uint64_t Size() const 
+    {
+        return distance(elements_.begin(), elements_.end());
+    }
+
+    bool IsEmpty() const 
+    {
+        return elements_.empty();
+    }
+
+private:
+    vector<Type> elements_;
+};
+
+template <typename Type>
+class Queue 
+{
+public:
+    void Push(const Type& element) 
+    {
+        // напишите реализацию
+    }
+    void Pop() 
+    {
+        // напишите реализацию
+    }
+    Type& Front() 
+    {
+        // напишите реализацию
+    }
+    uint64_t Size() const 
+    {
+        // напишите реализацию
+    }
+    bool IsEmpty() const 
+    {
+        // напишите реализацию
+    }
+
+private:
+    Stack<Type> stack1_;
+    Stack<Type> stack2_;
+};
+
+int main() 
+{
+    Queue<int> queue;
+    vector<int> values(5);
+
+    // заполняем вектор для тестирования очереди
+    iota(values.begin(), values.end(), 1);
+
+    // перемешиваем значения
+    random_shuffle(values.begin(), values.end());
+    PrintRange(values.begin(), values.end());
+    cout << "Заполняем очередь"s << endl;
+
+    // заполняем очередь и выводим элемент в начале очереди
+    for (int i = 0; i < 5; ++i) 
+    {
+        queue.Push(values[i]);
+        cout << "Вставленный элемент "s << values[i] << endl;
+        cout << "Первый элемент очереди "s << queue.Front() << endl;
+    }
+    cout << "Вынимаем элементы из очереди"s << endl;
+
+    // выводим элемент в начале очереди и вытаскиваем элементы по одному
+    while (!queue.IsEmpty()) 
+    {
+        // сначала будем проверять начальный элемент, а потом вытаскивать,
+        // так как операция Front на пустой очереди не определена
+        cout << "Будем вынимать элемент "s << queue.Front() << endl;
+        queue.Pop();
+    }
+    return 0;
+}
