d8354ad93a13d80acc8a24e55b49b2c3d6855dc0
CyberCat <Vgnetnet156@gmail.com>
2023-09-06 12:37:25 +0300

    Init task lesson 6

--- Lesson 3 Measuring time/main.cpp
+++ Lesson 3 Measuring time/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <chrono>
 #include <thread>
+
 #include "log_duration.h"
 
 using namespace std;
@@ -10,6 +11,7 @@ using namespace std;
 vector<int> ReverseVector(const vector<int>& source_vector) 
 {
     vector<int> res;
+
     for (int i : source_vector) 
     {
         res.insert(res.begin(), i);
@@ -18,72 +20,50 @@ vector<int> ReverseVector(const vector<int>& source_vector)
     return res;
 }
 
-int CountPops(const vector<int>& source_vector, int begin, int end) 
+vector<int> ReverseVector2(const vector<int>& source_vector) 
 {
-    int res = 0;
+    vector<int> res;
 
-    for (int i = begin; i < end; ++i) 
+    // будем проходить source_vector задом наперёд
+    // с помощью обратного итератора
+    for (auto iterator = source_vector.rbegin(); iterator != source_vector.rend(); ++iterator) 
     {
-        if (source_vector[i]) 
-        {
-            ++res;
-        }
+        res.push_back(*iterator);
     }
 
     return res;
 }
 
-void AppendRandom(vector<int>& v, int n) 
+vector<int> ReverseVector3(const vector<int>& source_vector) 
 {
-    for (int i = 0; i < n; ++i) 
-    {
-        // получаем случайное число с помощью функции rand.
-        // с помощью (rand() % 2) получим целое число в диапазоне 0..1.
-        // в C++ имеются более современные генераторы случайных чисел,
-        // но в данном уроке не будем их касаться
-        v.push_back(rand() % 2);
-    }
+    return {source_vector.rbegin(), source_vector.rend()};
+}
+
+vector<int> ReverseVector4(const vector<int>& source_vector) 
+{
+    vector<int> res(source_vector.size());
+
+    // реализация вашего собственного реверсирования
+
+    return res;
 }
 
 void Operate() 
 {
-    LOG_DURATION("Total"s);
-    vector<int> random_bits;
-    vector<int> reversed_bits;
+    vector<int> rand_vector;
+    int n;
 
-    // операция << для целых чисел это сдвиг всех бит в двоичной
-    // записи числа. Запишем с её помощью число 2 в степени 17 (131072)
-    static const int N = 1 << 17;
+    cin >> n;
+    rand_vector.reserve(n);
 
-    {
-        LOG_DURATION("Append random"s);
-        //LogDuration time_log("Append random"s);
-        // заполним вектор случайными числами 0 и 1
-        AppendRandom(random_bits, N);
+    for (int i = 0; i < n; ++i) {
+        rand_vector.push_back(rand());
     }
 
-    {
-        LOG_DURATION("Reverse"s);
-        // перевернём вектор задом наперёд
-        reversed_bits = ReverseVector(random_bits);
-    }
-
-    {
-        LOG_DURATION("Counting"s);
-        // посчитаем процент единиц на начальных отрезках вектора
-        for (int i = 1, step = 1; i <= N; i += step, step *= 2) 
-        {
-            // чтобы вычислить проценты, мы умножаем на литерал 100. типа double;
-            // целочисленное значение функции CountPops при этом автоматически
-            // преобразуется к double, как и i
-            double rate = CountPops(reversed_bits, 0, i) * 100. / i;
-            cout << "After "s << i << " bits we found "s << rate << "% pops"s << endl;
-        }
-    }
+    // код измерения тут
 }
 
-int main() 
-{
+int main() {
     Operate();
     return 0;
 }
