4c78ac130191602cac223a3e000a702b7a247015
CyberCat <Vgnetnet156@gmail.com>
2023-10-31 10:59:10 +0300

    Task init

---
+++ Lesson 6 Working with the file system/main.cpp
@@ -0,0 +1,38 @@
+#include <cassert>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+using filesystem::path;
+
+path operator""_p(const char* data, std::size_t sz) {
+    return path(data, data + sz);
+}
+
+// напишите эту функцию
+void PrintTree(ostream& dst, const path& p);
+
+int main() {
+    error_code err;
+    filesystem::remove_all("test_dir", err);
+    filesystem::create_directories("test_dir"_p / "a"_p, err);
+    filesystem::create_directories("test_dir"_p / "b"_p, err);
+
+    ofstream("test_dir"_p / "b"_p / "f1.txt"_p);
+    ofstream("test_dir"_p / "b"_p / "f2.txt"_p);
+    ofstream("test_dir"_p / "a"_p / "f3.txt"_p);
+
+    ostringstream out;
+    PrintTree(out, "test_dir"_p);
+    assert(out.str() ==
+        "test_dir\n"
+        "  b\n"
+        "    f2.txt\n"
+        "    f1.txt\n"
+        "  a\n"
+        "    f3.txt\n"s
+    );
+}
