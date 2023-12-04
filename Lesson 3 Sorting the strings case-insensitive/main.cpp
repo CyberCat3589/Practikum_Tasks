c7e5c12ef520a162e47612bf61546fa0c8084a17
CyberCat <Vgnetnet156@gmail.com>
2023-12-04 16:35:05 +0300

    Init task

---
+++ Lesson 3 Sorting the strings case-insensitive/main.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() 
+{
+    // считайте входные данные и сформируйте вывод программы
+    // ...
+}
