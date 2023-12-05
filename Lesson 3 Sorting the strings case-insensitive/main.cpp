403d4b18d883346ff680861d11e029d42a52c3be
CyberCat <Vgnetnet156@gmail.com>
2023-12-05 16:55:25 +0300

    task done

--- Lesson 3 Sorting the strings case-insensitive/main.cpp
+++ Lesson 3 Sorting the strings case-insensitive/main.cpp
@@ -7,6 +7,34 @@ using namespace std;
 
 int main() 
 {
-    // считайте входные данные и сформируйте вывод программы
-    // ...
+    vector<string> words; // массив строк
+    
+    int elementsNum; // кол-во элементов
+    cin >> elementsNum;
+
+    // ввод строк
+    while (elementsNum > 0)
+    {
+        string word;
+        cin >> word;
+        words.push_back(word);
+        --elementsNum;
+    }
+
+    sort(words.begin(), words.end(),
+    [](string& left, string& right)
+    {
+        return lexicographical_compare(
+            left.begin(), left.end(), 
+            right.begin(), right.end(),
+            [](char& left, char& right){ return tolower(static_cast<unsigned char>(left)) < tolower(static_cast<unsigned char>(right)); });
+
+    });
+
+    // вывод отсортированного массива
+    for(string word : words)
+    {
+        cout << word << " ";
+    }
+    
 }
