f100b6fa3ec1f21d3eac116ccb1fefe0eb66e7b4
CyberCat <Vgnetnet156@gmail.com>
2024-03-14 11:46:46 +0300

    Task done

--- Lesson 7 Working with arrays/main.cpp
+++ Lesson 7 Working with arrays/main.cpp
@@ -1,16 +1,37 @@
 #include <cassert>
 #include <vector>
+#include <cstddef>
+#include <iostream>
 
-template <typename T> void ReverseArray(T* start, size_t size)
+template <typename T> 
+void ReverseArray(T* start, size_t size)
 {
-    // Напишите тело функции самостоятельно
+    int j = size - 1;
+    int i = 0;
+    while(i < j)
+    {
+        T temp = *(start + i);
+        *(start + i) = *(start + j);
+        *(start + j) = temp;
+        ++i;
+        --j;
+    }
+}
+
+void Print(std::vector<int> nums)
+{
+    for(int num : nums)
+    {
+        std::cout << num << std::endl;
+    }
 }
 
 int main()
 {
     using namespace std;
 
-    vector<int> v = {1, 2};
+    vector<int> v = {1, 2, 3, 4, 5};
     ReverseArray(v.data(), v.size());
-    assert(v == (vector<int>{2, 1}));
+    //assert(v == (vector<int>{5, 4, 3, 2, 1}));
+    Print(v);
 }
