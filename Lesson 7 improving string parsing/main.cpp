14c8549e92085d18759941f513522675ac534e76
Kulikov Nikita <Vgnetnet156@gmail.com>
2024-03-16 17:51:17 +0300

    Init task

---
+++ Lesson 7 improving string parsing/main.cpp
@@ -0,0 +1,36 @@
+#include <cassert>
+#include <iostream>
+#include <string_view>
+#include <vector>
+
+using namespace std;
+
+vector<string_view> SplitIntoWordsView(string_view str)
+{
+    vector<string_view> result;
+    int64_t pos = str.find_first_not_of(" ");
+    const int64_t pos_end = str.npos;
+
+    while (pos != pos_end)
+    {
+        int64_t space = str.find(' ', pos);
+        result.push_back(space == pos_end ? str.substr(pos) : str.substr(pos, space - pos));
+        pos = str.find_first_not_of(" ", space);
+    }
+
+    return result;
+}
+
+int main()
+{
+    assert((SplitIntoWordsView("") == vector<string_view>{}));
+    assert((SplitIntoWordsView("     ") == vector<string_view>{}));
+    assert((SplitIntoWordsView("aaaaaaa") == vector{"aaaaaaa"sv}));
+    assert((SplitIntoWordsView("a") == vector{"a"sv}));
+    assert((SplitIntoWordsView("a b c") == vector{"a"sv, "b"sv, "c"sv}));
+    assert((SplitIntoWordsView("a    bbb   cc") == vector{"a"sv, "bbb"sv, "cc"sv}));
+    assert((SplitIntoWordsView("  a    bbb   cc") == vector{"a"sv, "bbb"sv, "cc"sv}));
+    assert((SplitIntoWordsView("a    bbb   cc   ") == vector{"a"sv, "bbb"sv, "cc"sv}));
+    assert((SplitIntoWordsView("  a    bbb   cc   ") == vector{"a"sv, "bbb"sv, "cc"sv}));
+    cout << "All OK" << endl;
+}
