c98d469b644e7bd2cfdabab87527dede893229fd
Kulikov Nikita <Vgnetnet156@gmail.com>
2024-03-16 18:34:40 +0300

    Task 1/2 done

--- Lesson 7 improving string parsing/main.cpp
+++ Lesson 7 improving string parsing/main.cpp
@@ -2,20 +2,22 @@
 #include <iostream>
 #include <string_view>
 #include <vector>
+#include <cstdint>
 
 using namespace std;
 
 vector<string_view> SplitIntoWordsView(string_view str)
 {
     vector<string_view> result;
-    int64_t pos = str.find_first_not_of(" ");
-    const int64_t pos_end = str.npos;
 
-    while (pos != pos_end)
+    str.remove_prefix(min(str.find_first_not_of(" "), str.size()));
+
+    while (!str.empty())
     {
-        int64_t space = str.find(' ', pos);
-        result.push_back(space == pos_end ? str.substr(pos) : str.substr(pos, space - pos));
-        pos = str.find_first_not_of(" ", space);
+        int64_t space = str.find(' ');
+        result.push_back(str.substr(0, space));
+        str.remove_prefix(min(str.find(" "), str.size()));
+        str.remove_prefix(min(str.find_first_not_of(" "), str.size()));
     }
 
     return result;
