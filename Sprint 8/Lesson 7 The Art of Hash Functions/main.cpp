9bb647b07c39865a5aff5acf5a218447a3b6d3fe
CyberCat <Vgnetnet156@gmail.com>
2024-04-08 12:47:08 +0300

    Task 2/2 done

--- Sprint 8/Lesson 7 The Art of Hash Functions/main.cpp
+++ Sprint 8/Lesson 7 The Art of Hash Functions/main.cpp
@@ -19,7 +19,26 @@ struct Dumbbell
 
 struct DumbbellHash
 {
-    // реализуйте хешер для Dumbbell
+    size_t operator()(const Dumbbell& dumbbell)
+    {
+        size_t h_c1_x = double_hasher(dumbbell.circle1.x);
+        size_t h_c1_y = double_hasher(dumbbell.circle1.y);
+        size_t h_c1_r = double_hasher(dumbbell.circle1.r);
+
+        size_t h_c1 = h_c1_x + h_c1_y * 22 + h_c1_r * 22 * 22;
+
+        size_t h_c2_x = double_hasher(dumbbell.circle2.x);
+        size_t h_c2_y = double_hasher(dumbbell.circle2.y);
+        size_t h_c2_r = double_hasher(dumbbell.circle2.r);
+
+        size_t h_c2 = h_c2_x + h_c2_y * 22 + h_c2_r * 22 * 22;
+
+        return (h_c1 + h_c2 * (22*22*22) + str_hasher(dumbbell.text));
+    }
+
+  private:
+    hash<string> str_hasher;
+    hash<double> double_hasher;
 };
 
 int main()
