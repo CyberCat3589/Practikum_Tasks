7152878375429ebf8f7b599a0f875e4a3bc3478b
Kulikov Nikita <Vgnetnet156@gmail.com>
2024-04-14 20:09:32 +0300

    Task done

--- Sprint 8/Lesson 4 Syntax of Namespaces/ini.cpp
+++ Sprint 8/Lesson 4 Syntax of Namespaces/ini.cpp
@@ -1,5 +1,4 @@
 #include "ini.h"
-#include <cassert>
 
 using namespace std;
 
@@ -7,14 +6,13 @@ namespace ini
 {
     Section& Document::AddSection(std::string name)
     {
-        Section new_section;
-        sections_[name] = new_section;
-        return new_section;
+        return sections_[name];
     }
 
     const Section& Document::GetSection(const std::string& name) const
     {
-        assert(sections_.find(name) != sections_.end());
+        static Section static_empty;
+        if(sections_.find(name) == sections_.end()) return static_empty;
         return sections_.at(name);
     }
 
@@ -25,6 +23,30 @@ namespace ini
 
     Document Load(std::istream& input)
     {
-        
+        Document result;
+        Section* section = nullptr;
+        for (string line; getline(input, line);)
+        {
+            auto pos = line.find_first_not_of(' ', 0);
+            if (pos == line.npos)
+            {
+                continue;
+            }
+            line = line.substr(pos);
+            if (line[0] == '[')
+            {
+                string section_name = line.substr(1, line.find(']') - 1);
+                section = &result.AddSection(section_name);
+            }
+            else
+            {
+                pos = line.find('=');
+                string unit_name = line.substr(0, line.find_last_not_of(' ', pos - 1) + 1);
+                line = line.substr(line.find_first_not_of(' ', pos + 1));
+                string unit_value = line.substr(0, line.find_last_not_of(' ') + 1);
+                section->insert({unit_name, unit_value});
+            }
+        }
+        return result;
     }
 }
