657e43d13199b1d6447b6995c3ac2e016d16bfd3
CyberCat <Vgnetnet156@gmail.com>
2024-05-07 16:55:04 +0300

    task done

--- Sprint 9/Lesson 4 Smart Pointer shared_ptr/shapes.h
+++ Sprint 9/Lesson 4 Smart Pointer shared_ptr/shapes.h
@@ -4,39 +4,82 @@
 #include <memory>
 
 // Поддерживаемые виды фигур: прямоугольник и эллипс
-enum class ShapeType { RECTANGLE, ELLIPSE};
+enum class ShapeType
+{
+    RECTANGLE,
+    ELLIPSE
+};
 
-class Shape {
-public:
+class Shape
+{
+  public:
     // Фигура после создания имеет нулевые координаты и размер,
     // а также не имеет текстуры
-    explicit Shape(ShapeType type) {
-        // Заглушка. Реализуйте конструктор самостоятельно
-        (void) type;
+    explicit Shape(ShapeType type) : shape_type_(type)
+    {
     }
 
-    void SetPosition(Point pos) {
-        (void) pos;
-        // Заглушка. Реализуйте метод самостоятельно
+    void SetPosition(Point pos)
+    {
+        position_ = pos;
     }
 
-    void SetSize(Size size) {
-        (void) size;
-        // Заглушка. Реализуйте метод самостоятельно
+    void SetSize(Size size)
+    {
+        size_ = size;
     }
 
-    void SetTexture(std::shared_ptr<Texture> texture) {
-        (void) texture;
-        // Заглушка. Реализуйте метод самостоятельно
+    void SetTexture(std::shared_ptr<Texture> texture)
+    {
+        texture_ = texture;
     }
 
     // Рисует фигуру на указанном изображении
-	// В зависимости от типа фигуры должен рисоваться либо эллипс, либо прямоугольник
+    // В зависимости от типа фигуры должен рисоваться либо эллипс, либо прямоугольник
     // Пиксели фигуры, выходящие за пределы текстуры, а также в случае, когда текстура не задана,
     // должны отображаться с помощью символа точка '.'
     // Части фигуры, выходящие за границы объекта image, должны отбрасываться.
-    void Draw(Image& image) const {
-        (void) image;
-        // Заглушка. Реализуйте метод самостоятельно
+    void Draw(Image& image) const
+    {
+        const auto image_size = GetImageSize(image);
+ 
+        int y_ = 0;
+        int x_ = 0;
+        char helper;
+ 
+        for (int y = 0; y < size_.height; ++y) {
+            for (int x = 0; x < size_.width; ++x) {
+ 
+                y_ = position_.y + y;
+                x_ = position_.x + x;
+ 
+                if (y_ < 0 || 
+                    x_ < 0 || 
+                    y_ >= image_size.height || 
+                    x_ >= image_size.width) {
+ 
+                    continue;
+                }
+ 
+                if (!texture_) {
+                    helper = '.';
+                } else {
+                    helper = texture_->GetPixelColor({x, y});
+                }
+ 
+                if ((shape_type_ == ShapeType::ELLIPSE 
+                              && IsPointInEllipse({x, y}, size_)) 
+                     || (shape_type_ == ShapeType::RECTANGLE)) {
+ 
+                    image[y_][x_] = helper;
+                }
+            }
+        }
     }
+
+  private:
+    ShapeType shape_type_ = ShapeType::RECTANGLE;
+    Point position_ = {0, 0};
+    Size size_ = {0, 0};
+    std::shared_ptr<Texture> texture_ = nullptr;
 };
--- Sprint 9/Lesson 4 Smart Pointer shared_ptr/texture.h
+++ Sprint 9/Lesson 4 Smart Pointer shared_ptr/texture.h
@@ -1,23 +1,26 @@
 #pragma once
 #include "common.h"
-
+ 
 class Texture {
 public:
     explicit Texture(Image image)
         : image_(std::move(image)) {
     }
-
+ 
     Size GetSize() const {
-        // Заглушка. Реализуйте метод самостоятельно
-        return {0, 0};
+        if (image_.empty()) {
+            return {0, 0};
+        } 
+        return {int(image_[0].size()), int(image_.size())};
     }
-
+ 
     char GetPixelColor(Point p) const {
-        (void) p;
-        // Заглушка. Реализуйте метод самостоятельно
-        return ' ';
+        if (int(image_.size()) <= p.y || int(image_[0].size()) <= p.x || p.x < 0 || p.y < 0) {
+            return '.';
+        }
+        return image_[p.y][p.x];
     }
-
+ 
 private:
     Image image_;
 };
