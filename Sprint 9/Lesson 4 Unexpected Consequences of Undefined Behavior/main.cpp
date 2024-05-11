fb55f888e9087693b5931b774553dd7b2ecb4d76
CyberCat <Vgnetnet156@gmail.com>
2024-05-11 18:44:28 +0300

    Init task

---
+++ Sprint 9/Lesson 4 Unexpected Consequences of Undefined Behavior/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string_view>
+
+using namespace std;
+
+// clang-format off
+
+const int NUM_PLANETS = 9;
+const string_view PLANETS[] = {
+    "Mercury"sv, "Venus"sv, "Earth"sv,
+    "Mars"sv, "Jupiter"sv, "Saturn"sv,
+    "Uranus"sv, "Neptune"sv,
+};
+
+// clang-format on
+
+bool IsPlanet(string_view name)
+{
+    for (int i = 0; i < NUM_PLANETS; ++i)
+    {
+        if (PLANETS[i] == name)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void Test(string_view name)
+{
+    cout << name << " is " << (IsPlanet(name) ? ""sv : "NOT "sv) << "a planet"sv << endl;
+}
+
+int main()
+{
+    Test("Earth"sv);
+    Test("Jupiter"sv);
+    Test("Pluto"sv);
+    Test("Moon"sv);
+}
