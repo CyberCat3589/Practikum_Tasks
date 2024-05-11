0059cb93529e76d9d3d1bedcbf49d82e963ebfe3
Kulikov Nikita <Vgnetnet156@gmail.com>
2024-05-11 17:26:44 +0300

    Task done

--- Sprint 9/Lesson 3 Performing Optimizations/main.cpp
+++ Sprint 9/Lesson 3 Performing Optimizations/main.cpp
@@ -1,12 +1,39 @@
 #include <cstdint>
 #include <iostream>
+#include <limits>
+#include <optional>
 
 using namespace std;
+
+template <typename T> optional<T> SafeAdd(T a, T b)
+{
+    if (a > 0 && b > 0)
+    {
+        if (a > numeric_limits<T>::max() - b)
+        {
+            return nullopt;
+        }
+    }
+    else if (a < 0 && b < 0)
+    {
+        if (a < numeric_limits<T>::min() - b)
+        {
+            return nullopt;
+        }
+    }
+    return a + b;
+}
 
 int main()
 {
     int64_t a;
     int64_t b;
     cin >> a >> b;
-    cout << a + b << endl;
+    auto sum = SafeAdd(a, b);
+    if (sum.has_value())
+    {
+        cout << sum.value();
+    }
+    else
+        cout << "Overflow!" << endl;
 }
