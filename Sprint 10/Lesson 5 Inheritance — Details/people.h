849d5d14eaa4acbeb3fa5c203a3bb27044412bce
CyberCat <Vgnetnet156@gmail.com>
2024-05-21 10:21:31 +0300

    Task done

--- Sprint 10/Lesson 5 Inheritance — Details/people.h
+++ Sprint 10/Lesson 5 Inheritance — Details/people.h
@@ -5,14 +5,13 @@
 class Person;
 
 // Наблюдатель за состоянием человека.
-class PersonObserver {
-public:
+class PersonObserver
+{
+  public:
     // Этот метод вызывается, когда меняется состояние удовлетворённости человека
-    void OnSatisfactionChanged(Person& /*person*/, int /*old_value*/, int /*new_value*/) {
-        // Реализация метода базового класса ничего не делает
-    }
+    virtual void OnSatisfactionChanged(Person& /*person*/, int /*old_value*/, int /*new_value*/){}
 
-protected:
+  protected:
     // Класс PersonObserver не предназначен для удаления напрямую
     ~PersonObserver() = default;
 };
@@ -22,17 +21,20 @@ protected:
     При изменении уровня удовлетворённости уведомляет
     связанного с ним наблюдателя
 */
-class Person {
-public:
-    Person(const std::string& name, int age) {
-        // Напишите реализацию самостоятельно
-    }
+class Person
+{
+  public:
+    Person(const std::string& name, int age) : name_(name), age_(age){}
 
-    int GetSatisfaction() const {
+    virtual ~Person(){}
+
+    int GetSatisfaction() const
+    {
         return satisfaction_;
     }
 
-    const std::string& GetName() const {
+    const std::string& GetName() const
+    {
         return name_;
     }
 
@@ -40,79 +42,119 @@ public:
     // уведомляется об изменении уровня удовлетворённости человека
     // Новый наблюдатель заменяет собой ранее привязанного
     // Если передать nullptr в качестве наблюдателя, это эквивалентно отсутствию наблюдателя
-    void SetObserver(PersonObserver* observer) {
-        // Напишите реализацию самостоятельно
+    void SetObserver(PersonObserver* observer)
+    {
+        observer_ = observer;
     }
 
-    int GetAge() const {
+    int GetAge() const
+    {
         return age_;
     }
 
     // Увеличивает на 1 количество походов на танцы
     // Увеличивает удовлетворённость на 1
-    void Dance() {
-        // Напишите тело метода самостоятельно
+    virtual void Dance()
+    {
+        ++dance_count_;
+        SetSatisfaction(GetSatisfaction() + 1);
     }
 
-    int GetDanceCount() const {
-        // Заглушка. Напишите реализацию самостоятельно
-        return 0;
+    int GetDanceCount() const
+    {
+        return dance_count_;
     }
 
     // Прожить день. Реализация в базовом классе ничего не делает
-    void LiveADay() {
-        // Подклассы могут переопределить этот метод
-    }
+    virtual void LiveADay(){}
 
-private:
+  protected:
+  virtual void SetSatisfaction(int value) 
+  {
+        if (satisfaction_ != value) 
+        {
+            int old_value = satisfaction_;
+            satisfaction_ = value;
+            if (observer_) 
+            {
+                observer_->OnSatisfactionChanged(*this, old_value, satisfaction_);
+            }
+        }
+    }
+    
+
+  private:
     std::string name_;
+    int age_;
     PersonObserver* observer_ = nullptr;
     int satisfaction_ = 100;
-    int age_;
+    int dance_count_ = 0;
 };
 
 // Рабочий.
 // День рабочего проходит за работой
-class Worker {
-public:
-    Worker(const std::string& name, int age) {
-        // Напишите недостающий код
-    }
+class Worker : public Person
+{
+  public:
+    Worker(const std::string& name, int age) : Person(name, age){}
 
     // Рабочий старше 30 лет и младше 40 за танец получает 2 единицы удовлетворённости вместо 1
+    void Dance() override
+    {
+        Person::Dance();
+        if(int age = GetAge(); age > 30 && age < 40)
+        {
+            SetSatisfaction(GetSatisfaction() + 1);
+        }
+    }
 
     // День рабочего проходит за работой
+    void LiveADay() override
+    {
+        Work();
+    }
 
     // Увеличивает счётчик сделанной работы на 1, уменьшает удовлетворённость на 5
-    void Work() {
-        // Напишите тело метода самостоятельно
+    void Work()
+    {
+        ++work_count_;
+        SetSatisfaction(GetSatisfaction() - 5);
     }
 
     // Возвращает значение счётчика сделанной работы
-    int GetWorkDone() const {
-        // Заглушка. Напишите реализацию самостоятельно
-        return 0;
+    int GetWorkDone() const
+    {
+        return work_count_;
     }
+  private:
+    int work_count_ = 0;
 };
 
 // Студент.
 // День студента проходит за учёбой
-class Student {
-public:
-    Student(const std::string& name, int age) {
-        // Напишите недостающий код
-    }
+class Student : public Person
+{
+  public:
+    Student(const std::string& name, int age) : Person(name, age){}
 
     // День студента проходит за учёбой
+    void LiveADay() override
+    {
+        Study();
+    }
 
     // Учёба увеличивает уровень знаний на 1, уменьшает уровень удовлетворённости на 3
-    void Study() {
-        // Напишите реализацию самостоятельно
+    void Study()
+    {
+        ++knowledge_level_;
+        SetSatisfaction(GetSatisfaction() - 3);
     }
 
     // Возвращает уровень знаний
-    int GetKnowledgeLevel() const {
-        // Заглушка, напишите реализацию самостоятельно
-        return 0;
+    int GetKnowledgeLevel() const
+    {
+        return knowledge_level_;
     }
+  private:
+    int knowledge_level_ = 0;
 };
--- Sprint 10/Lesson 5 Inheritance — Details/supervisor.h
+++ Sprint 10/Lesson 5 Inheritance — Details/supervisor.h
@@ -9,12 +9,28 @@
     побуждает человека танцевать до тех пор, пока уровень уровень удовлетворённости
     не станет больше или равен максимальному значению
 */
-class SatisfactionSupervisor {
+class SatisfactionSupervisor : public PersonObserver
+{
 public:
     // Конструктор принимает значение нижнего и верхнего уровня удовлетворённости
-    SatisfactionSupervisor(int min_satisfaction, int max_satisfaction) {
-        // Напишите реализацию самостоятельно
+    SatisfactionSupervisor(int min_satisfaction, int max_satisfaction) 
+        : min_satisfaction_(min_satisfaction), max_satisfaction_(max_satisfaction){}
+
+    void OnSatisfactionChanged(Person& person, int old_value, int new_value) 
+    {
+ 
+        if (new_value < min_satisfaction_ && (new_value - old_value) < 0) 
+        {
+ 
+            while (person.GetSatisfaction() < max_satisfaction_) 
+            {
+                person.Dance();
+            }
+        }
     }
 
     // Напишите реализацию самостоятельно
+  private:
+    int min_satisfaction_ = 0;
+    int max_satisfaction_ = 0;
 };
